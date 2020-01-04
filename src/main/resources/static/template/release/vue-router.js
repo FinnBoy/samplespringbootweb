04fd5e81a78bfef81de4fb5ac1d65e60421f4438
FinnBoy <finn0918@163.com>
2020-01-05 01:39:50 +0800

    Update

---
+++ src/main/resources/static/template/release/vue-router.js
@@ -0,0 +1,99 @@
+
+var UploadView = {
+    data: function () {
+        return {}
+    },
+    template: '#upload-view'
+}
+var WorkbenchView = {
+    data: function () {
+        return {}
+    },
+    template: '#workbench-view'
+}
+var componentC = {
+    data: function () {
+        return {}
+    },
+    template: '<span>setting</span>'
+}
+
+let router = new VueRouter({
+    routes: [
+        {
+            path: '/upload',
+            component: UploadView
+        },
+        {
+            path: '/workbench/agent',
+            component: WorkbenchView
+        },
+        {
+            path: '/setting/auth',
+            component: componentC
+        }
+    ]
+})
+
+new Vue({
+    el: '#app',
+    router: router,
+    data: function () {
+        return {
+            collapsed: true,
+            show: true,
+            drawer: false,
+            asideWidth: '50px',
+            routes: [
+                {
+                    id: 1,
+                    leaf: true,
+                    disabled: false,
+                    icon: 'el-icon-tickets',
+                    name: '待上传',
+                    path: '/upload'
+                },
+                {
+                    id: 2,
+                    leaf: false,
+                    disabled: false,
+                    icon: 'el-icon-menu',
+                    name: '工作台',
+                    path: '/workbench',
+                    children: [
+                        {id: 21, path: '/agent', disabled: false, name: '代理商'},
+                        {id: 22, path: '/parts', disabled: false, name: '零部件'}
+                    ]
+                },
+                {
+                    id: 3,
+                    leaf: false,
+                    disabled: false,
+                    icon: 'el-icon-setting',
+                    name: '设定',
+                    path: '/setting',
+                    children: [
+                        {id: 31, path: '/auth', disabled: false, name: '权限管理'},
+                        {id: 32, path: '/basic', disabled: false, name: '基础设定'}
+                    ]
+                },
+            ],
+        }
+    },
+    methods: {
+        toggleCollapsed: function () {
+            var val = !this.collapsed;
+            if (val) {
+                this.asideWidth = '50px';
+                this.collapsed = val;
+            } else {
+                this.collapsed = val;
+                this.asideWidth = '240px'; // 300px
+            }
+
+        },
+    },
+    components: {
+
+    }
+})
