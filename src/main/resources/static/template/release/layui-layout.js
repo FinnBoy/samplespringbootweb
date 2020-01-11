8a2538eb046089ea922b311fad31aa7b38f6bb6b
FinnBoy <finn0918@163.com>
2020-01-11 21:10:17 +0800

    Update

---
+++ src/main/resources/static/template/release/layui-layout.js
@@ -0,0 +1,10 @@
+
+layui.use(['element', 'layer', 'form', 'table', 'tree'], function() {
+    var element, layer, form, table, tree;
+
+    var element = layui.element;
+    var layer = layui.layer;
+    var form = layui.form;
+    var table = layui.table;
+    var tree = layui.tree;
+});
