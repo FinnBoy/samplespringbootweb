a2df55c01930c33d177ff933f9030147142b0b5b
FinnBoy <finn0918@163.com>
2020-01-11 22:21:45 +0800

    Update

--- src/main/resources/static/template/release/layui-layout.js
+++ src/main/resources/static/template/release/layui-layout.js
@@ -1,10 +1,29 @@
 
-layui.use(['element', 'layer', 'form', 'table', 'tree'], function() {
-    var element, layer, form, table, tree;
+layui.use(['element', 'layer', 'form', 'table', 'tree', 'jquery'], function() {
+    var element, layer, form, table, tree, $;
 
     var element = layui.element;
     var layer = layui.layer;
     var form = layui.form;
     var table = layui.table;
     var tree = layui.tree;
+    var $ = layui.$;
+
+    $('#hideBtn').hide();
+
+    $('#showBtn').click(function () {
+        $('body').removeClass('finn-collapsed');
+        $(this).hide();
+        $('#hideBtn').show();
+        element.render('nav', 'head');
+    });
+
+    $('#hideBtn').click(function () {
+        $('body').addClass('finn-collapsed');
+        $(this).hide();
+        $('#showBtn').show();
+        element.render('nav', 'head');
+    });
+
+    $('#showBtn').trigger('click');
 });
