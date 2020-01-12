78d39585b8675e2e3d3a89cf3f41da7c561c2595
FinnBoy <finn0918@163.com>
2020-01-12 17:43:36 +0800

    Update

--- src/main/resources/static/template/release/layui-layout.js
+++ src/main/resources/static/template/release/layui-layout.js
@@ -1,12 +1,13 @@
 
-layui.use(['element', 'layer', 'form', 'table', 'tree', 'jquery'], function() {
-    var element, layer, form, table, tree, $;
+layui.use(['element', 'layer', 'form', 'table', 'tree', 'laytpl', 'jquery'], function() {
+    var element, layer, form, table, tree, laytpl, $;
 
     var element = layui.element;
     var layer = layui.layer;
     var form = layui.form;
     var table = layui.table;
     var tree = layui.tree;
+    var laytpl = layui.laytpl;
     var $ = layui.$;
 
     var $hideBtn = $('#hideBtn');
@@ -26,7 +27,7 @@ layui.use(['element', 'layer', 'form', 'table', 'tree', 'jquery'], function() {
 
         $(this).hide();
         $hideBtn.show();
-        element.render('nav', 'head');
+        element.render('nav', 'head-left-menu');
     });
 
     $hideBtn.click(function () {
@@ -35,7 +36,7 @@ layui.use(['element', 'layer', 'form', 'table', 'tree', 'jquery'], function() {
 
         $(this).hide();
         $showBtn.show();
-        element.render('nav', 'head');
+        element.render('nav', 'head-left-menu');
 
         $layout.addClass('finn-item-shrink');
     });
@@ -44,4 +45,45 @@ layui.use(['element', 'layer', 'form', 'table', 'tree', 'jquery'], function() {
         $showBtn.trigger('click');
     });
 
+    var menuList = [
+        {
+            icon: 'layui-icon-template',
+            title: '页面',
+            children: [
+                {
+                    title: '页面一'
+                },
+                {
+                    title: '页面二'
+                }
+            ]
+        },
+        {
+            icon: 'layui-icon-set',
+            title: '设定',
+            children: [
+                {
+                    title: '系统设定'
+                },
+                {
+                    title: '我的设定'
+                }
+            ]
+        },
+        {
+            icon: 'layui-icon-auz',
+            title: '授权'
+        }
+    ];
+
+    var $template = $('#side-menu-template')[0].innerHTML;
+    laytpl($template).render({menuList: menuList}, function(html) {
+        var $sample = $('.layui-side').find('.layui-nav');
+        var $container = $sample.parent();
+
+        $sample.remove();
+        $container.append(html);
+
+        element.render('nav', 'side-menu');
+    });
 });
